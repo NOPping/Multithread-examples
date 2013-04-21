77d67085d1b31a1de132828e2029510ec8ef88a2
Ian Duffy <ian@ianduffy.ie>
2013-04-21 21:00:34 +0100

    Update docs

---
+++ ChatServer/class_chat_server_1_1_consumer.js
@@ -0,0 +1,10 @@
+var class_chat_server_1_1_consumer =
+[
+    [ "addMessage", "class_chat_server_1_1_consumer.html#a84d3c7c0cb2ff19cb929f6d8171fb989", null ],
+    [ "close", "class_chat_server_1_1_consumer.html#a6737fd278d4ed720aedcfbb31d2c0d7e", null ],
+    [ "getNextMessage", "class_chat_server_1_1_consumer.html#ab64ac6276ce8ecfca9839e5d3c748d1f", null ],
+    [ "run", "class_chat_server_1_1_consumer.html#a691615e1d6f0310dcd0bb6d674005c2b", null ],
+    [ "sendMessage", "class_chat_server_1_1_consumer.html#a8800fdab85ab5a5e5cba90fd5dd2d584", null ],
+    [ "buffer", "class_chat_server_1_1_consumer.html#aba25f3f4d1c272a11818e17dcf1a9ab3", null ],
+    [ "outstream", "class_chat_server_1_1_consumer.html#a448da776a21cd7d6fee28b3831183287", null ]
+];
---
+++ ChatServer/files.js
@@ -0,0 +1,10 @@
+var files =
+[
+    [ "ChatServer.java", "_chat_server_8java.html", [
+      [ "ChatServer", "class_chat_server.html", "class_chat_server" ],
+      [ "MessageServer", "class_chat_server_1_1_message_server.html", "class_chat_server_1_1_message_server" ],
+      [ "Connection", "class_chat_server_1_1_connection.html", "class_chat_server_1_1_connection" ],
+      [ "Consumer", "class_chat_server_1_1_consumer.html", "class_chat_server_1_1_consumer" ],
+      [ "Producer", "class_chat_server_1_1_producer.html", "class_chat_server_1_1_producer" ]
+    ] ]
+];
